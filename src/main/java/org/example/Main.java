79507ae79ba8e0ee9d7e46a18602a739f67a674a
rockpraneeth1999 <rockpraneeth1999@gmail.com>
2026-02-07 21:48:22 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println(add(1, 2));
+    }
+
+    private static int add(int a, int b){
+        return a + b;
+    }
+}
